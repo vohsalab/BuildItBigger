e440b5897fc8170f6862e33c9ad3462f7073b264
Ivan Balashov <sputnic1985@gmail.com>
2016-03-03 00:16:24 -0500

    Initial commit

---
+++ androidjokelibrary/src/main/java/com/firstsputnik/androidjokelibrary/JokeActivity.java
@@ -0,0 +1,19 @@
+package com.firstsputnik.androidjokelibrary;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class JokeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joke);
+        Intent intent = getIntent();
+        String joke = intent.getStringExtra("Joke");
+        TextView tv = (TextView) findViewById(R.id.joke_textview);
+        tv.setText(joke);
+    }
+}
---
+++ jokerlib/src/main/java/com/firstsputnik/joker/Joker.java
@@ -0,0 +1,7 @@
+package com.firstsputnik.joker;
+
+public class Joker {
+    public String tellJoke() {
+        return "Here is my very funny joke";
+    }
+}
